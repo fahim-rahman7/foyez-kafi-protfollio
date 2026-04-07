5be42e0d83fb69c4f325232c2a5e15c46068dc79
foyezkafi <foyezkafi@gmail.com>
2026-04-07 23:43:09 +0600

    dsbgdksguhsdfjhkgsdfjhkgsdfjhkgsdf

---
+++ src/components/introduction/Introduction.jsx
@@ -0,0 +1,77 @@
+
+
+import kafi from "../../assets/kafiimages/kafi.jpeg"
+import "./introduction.css";
+import InformationSummary from "./InformationSummary";
+import { Link } from "react-router-dom";
+
+// Information summary data
+const informationSummaryData = [
+  {
+    id: 1,
+    title: "Experience",
+    description: "3 Y.",
+  },
+  {
+    id: 2,
+    title: "Projects Completed",
+    description: "60+",
+  },
+  {
+    id: 3,
+    title: "Happy Clients",
+    description: "48",
+  },
+];
+
+const Introduction = () => {
+  return (
+    <div
+      className="flex max-lg:flex-col-reverse sm:justify-between pt-10 lg:pt-31.5 lg:mb-27.5 max-xl:gap-2 p-2 max-xxl:px-4"
+      id="introduction"
+    >
+      <div className="w-full flex flex-col justify-between max-lg:text-center">
+        <div className="pt-13 me-31.5 w-full lg:w-auto transition-all duration-500">
+          <p className="text-3xl xxs:text-4xl sm:max-xl:text-5xl xl:text-6xl font-semibold w-full">
+            Hello, I’m
+            <span className="text-nowrap shrink-0 inline-block w-full">
+              Foyez kafi
+            </span>
+          </p>
+          <p className="text-xs xxs:text-lg lg:text-[18px] my-6">
+            I'm a  <span className="bg-highlight">MERN Stack Developer</span>{" "}
+            and the Founder & CEO of  <span className="bg-highlight"> DoctypeSolution </span>I help startups, entrepreneurs, 
+            and businesses build modern, scalable, and high-performance web applications. My expertise lies in crafting clean code, 
+            intuitive user experiences, and digital solutions that drive real business growth.
+          </p>
+          <p className="text-center lg:text-start">
+            <Link
+              className="btn-primary btn btn-xs xxs:btn-lg text-white"
+              href="tel:+88 01319877320"
+            >
+              Say Hello!
+            </Link>
+          </p>
+        </div>
+        <div className="mx-auto lg:mx-0 relative">
+          <div className="grid max-xxs:grid-flow-col grid-cols-3 w-fit mt-10 gap-1">
+            {informationSummaryData.map((item) => (
+              <InformationSummary key={item.id} item={item} />
+            ))}
+          </div>
+        </div>
+      </div>
+      <div
+        className={`max-w-134 w-full h-full max-lg:mx-auto aspect-[536/636] relative`}
+      >
+        <img
+          className={`shadow-2xl shadow-gray-200 w-full h-full absolute bottom-0 object-cover bg-white rounded-3xl`}
+          src={kafi}
+          alt="person"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Introduction;
---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,79 @@
+import person from "../../assets/images/person2.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import SocialMedia from "../common/socialMedia/SocialMedia";
+
+const Profile = () => {
+  return (
+    <div
+      className={`relative mx-4 xxl:mx-0.5 -bottom-20 lg:-bottom-28 z-10 rounded-2xl bg-white drop-shadow-2xl max-xl:mb-5 shadow-white xl:p-28 lg:p-20 md:p-16 sm:p-10 p-4`}
+      id="profile"
+    >
+      <div className="flex max-md:flex-col justify-between items-center gap-6">
+        {/* Profile image */}
+        <div className="xxl:max-w-106 w-auto h-auto xxl:max-h-126">
+          <div className="max-w-106 h-117 object-fill overflow-hidden rounded-xl">
+            <img
+              className="bg-soft-white h-[120%] object-cover"
+              src={person}
+              alt=""
+            />
+          </div>
+          {/* Social media section */}
+          <div className="relative bottom-9">
+            <div className="flex justify-center">
+              <div className="px-6 max-w-66 py-3 z-50 text-center bg-white rounded-[4px] center shadow-2xl drop-shadow-2xl shadow-white">
+                <SocialMedia />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="max-sm:w-full w-[33rem]">
+          <h2
+            className={`text-2xl xxs:text-3xl sm:text-4xl lg:text-[38px] text-[min(24px,38px)] max-md:text-center font-semibold mb-8`}
+          >
+            Building Scalable Web Applications with Modern Technologies
+          </h2>
+          <div
+            className={`text-xs xs:text-[16px] lg:text-lg font-normal max-md:text-center text-gray-600`}
+          >
+            <p className={``}>
+              I help businesses turn their ideas into powerful digital
+               products through clean code, modern design, and scalable 
+               technology. With a strong focus on performance, usability,
+                and innovation, I create web solutions that are not only 
+                visually appealing but also highly functional and reliable.
+            </p>
+            <p className="mt-3">
+            My goal is to build fast, responsive, and user-friendly web applications
+             that deliver seamless experiences across all devices. By combining modern
+              technologies with creative problem-solving, I create digital solutions that
+               help businesses grow and strengthen their online presence.
+            </p>
+          </div>
+          <div className="mt-8 flex max-md:justify-center">
+            <a
+              className="btn xxs:btn-lg px-6 max-xs:px-2 xxs:py-3 btn-primary text-xs xxs:text-[14px] sm:text-[16px]"
+              href="#!"
+            >
+              My Projects
+            </a>
+            <a
+              className={`btn xxs:btn-lg px-6 max-xs:px-2 xxs:py-3 hover:border-picto-primary bg-white duration-300 transition-all hover:text-picto-primary  ms-4 text-xs xxs:text-[14px] sm:text-[16px]`}
+              href="#!"
+            >
+              <FontAwesomeIcon icon={faDownload} /> Download CV
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+
+    
+  );
+};
+
+export default Profile;
